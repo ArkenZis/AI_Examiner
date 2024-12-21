28df9fc2ead790473f1b78b5a68b7948f832482a
i3130002 <i3130002@gmail.com>
2024-12-21 12:22:24 +0400

    Sample groq

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 st.title("Hello World")
 
@@ -12,3 +13,24 @@ if st.button("Submit"):
     st.write(question)
     st.write(reference)
     st.write(marking)
+
+@st.cache_data()
+def ask_llm(question, model="llama3-8b-8192"):
+    from groq import Groq
+    client = Groq(
+        api_key=os.environ.get(os.environ.get("GROQ_API_KEY")),
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": question,
+            }
+        ],
+        model=model,
+    )
+    st.write(chat_completion.choices[0].message.content)
+
+
+ask_llm("Hi")
